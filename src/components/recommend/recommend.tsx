40c2c67b1b6bbbfad7f5c0dcc5de2c14966ec33f
ICX\anastasiya.chultsova <nchulczova@gmail.com>
2021-11-01 13:16:28 +0300

    feat: card without dynamic creating

--- src/components/recommend/recommend.tsx
+++ src/components/recommend/recommend.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react'
 import './recommend.scss'
-import { Card } from 'react-bootstrap'
+import { Card} from 'react-bootstrap'
 
 import movie1 from './recmovies/movie1.jpg'
 import movie2 from './recmovies/movie2.jpeg'
@@ -9,41 +9,50 @@ import movie4 from './recmovies/movie4.jpg'
 import movie5 from './recmovies/movie5.jpg'
 import movie6 from './recmovies/movie6.jpg'
 
-interface Icard {
-    image: string;
-    link: string;
-}
-
-const Recommend: FC = () => {
-    
-    
-    const cardInfo = [
-        {image: { movie1 }, link: "Капитан Марвел"}, 
-        {image: { movie2 }, link: "Черная вдова"},
-        {image: { movie3 }, link: "Гарри Поттер"},
-        {image: { movie4 }, link: "Сталинград"},
-        {image: { movie5 }, link: "Неудержимые"},
-        {image: { movie6 }, link: "Начало"}
-    ]
-
-    const renderCard = (card: Icard [], index: number) => {
-        return (
-            <Card style = {{'display': 'inline-block','color': 'white', 'background': '#141414', 'border': 'solid black', 'width': '12rem'}} key = {index} className = "mb-3 text-center">
-                    <Card.Img src = { card.image }  variant = "top"/>
-                    <Card.Body>
-                    <Card.Link href = "#" style = {{'color': 'white'}}> { card.link }</Card.Link>
-                    </Card.Body>
-                </Card>
-        )
-    }
-    return (
+const Recommend: FC = () => ( 
+    <>
         <div className = "recommend">
             <a> 
                 <h3 className = "rec">Рекомендуем:</h3>
             </a>
-            {cardInfo.map(renderCard)}
-        </div>    
-    );
-};
+            <Card style = {{'color': 'white', 'background': '#141414', 'border': 'solid black', 'width': '12rem'}} className = "mb-3 text-center ">
+                    <Card.Img src = { movie1 }  variant = "top"/>
+                    <Card.Body>
+                    <Card.Link href = "#" style = {{'color': 'white'}}>Капитан Марвел</Card.Link>
+                    </Card.Body>
+                </Card>
+                <Card style = {{'color': 'white', 'background': '#141414', 'border': 'solid black', 'width': '12rem'}} className = "mb-3">
+                    <Card.Img src = { movie2 }  variant = "top"/>
+                    <Card.Body>
+                    <Card.Link href = "#" style = {{'color': 'white'}}>Черная вдова</Card.Link>
+                    </Card.Body>
+                </Card>
+                <Card style = {{'color': 'white', 'background': '#141414', 'border': 'solid black', 'width': '12rem'}} className = "mb-3">
+                    <Card.Img src = { movie3 }  variant = "top"/>
+                    <Card.Body>
+                    <Card.Link href = "#" style = {{'color': 'white'}}>Гарри Поттер</Card.Link>
+                    </Card.Body>
+                </Card>
+                <Card style = {{'color': 'white', 'background': '#141414', 'border': 'solid black', 'width': '12rem'}} className = "mb-3">
+                    <Card.Img src = { movie4 }  variant = "top"/>
+                    <Card.Body>
+                    <Card.Link href = "#" style = {{'color': 'white'}}>Сталинград</Card.Link>
+                    </Card.Body>
+                </Card>
+                <Card style = {{'color': 'white', 'background': '#141414', 'border': 'solid black', 'width': '12rem'}} className = "mb-3">
+                    <Card.Img src = { movie5 }  variant = "top"/>
+                    <Card.Body>
+                    <Card.Link href = "#" style = {{'color': 'white'}}>Неудержимые</Card.Link>
+                    </Card.Body>
+                </Card>
+                <Card style = {{'color': 'white', 'background': '#141414', 'border': 'solid black', 'width': '12rem'}} className = "mb-3">
+                    <Card.Img src = { movie6 }  variant = "top"/>
+                    <Card.Body>
+                    <Card.Link href = "#" style = {{'color': 'white'}}>Начало</Card.Link>
+                    </Card.Body>
+                </Card>  
+        </div>
+    </>    
+)
 
 export default Recommend
