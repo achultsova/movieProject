6d0a0f991906f51fa0558993dacdda0a8e2a4ca1
ICX\anastasiya.chultsova <nchulczova@gmail.com>
2021-11-01 13:46:06 +0300

    fix: try to add dynamic info for cards

--- src/components/recommend/recommend.tsx
+++ src/components/recommend/recommend.tsx
@@ -9,50 +9,44 @@ import movie4 from './recmovies/movie4.jpg'
 import movie5 from './recmovies/movie5.jpg'
 import movie6 from './recmovies/movie6.jpg'
 
-const Recommend: FC = () => ( 
-    <>
+interface Icard {
+    image: string;
+    link: string;
+}
+
+const Recommend: FC = () => {
+    const cardInfo: Icard[] = [
+        {image: movie1, link: "Капитан Марвел"},
+        {image: movie2, link: "Черная вдова"},
+        {image: movie3, link: "Гарри Поттер"},
+        {image: movie4, link: "Сталинград"},
+        {image: movie5, link: "Неудержимые"},
+        {image: movie6, link: "Начало"},
+    ]
+
+    const RenderCard: FC<{ card: Icard }> = ({ card }) => {
+        return (
+            <Card style = {{'color': 'white', 'background': '#141414', 'border': 'solid black', 'width': '12rem'}} className = "mb-3 text-center ">
+                    <Card.Img src = { card.image }  variant = "top"/>
+                    <Card.Body>
+                    <Card.Link href = "#" style = {{'color': 'white'}}> { card.link } </Card.Link>
+                    </Card.Body>
+                </Card>
+        )
+    }
+
+    return (
         <div className = "recommend">
             <a> 
                 <h3 className = "rec">Рекомендуем:</h3>
             </a>
-            <Card style = {{'color': 'white', 'background': '#141414', 'border': 'solid black', 'width': '12rem'}} className = "mb-3 text-center ">
-                    <Card.Img src = { movie1 }  variant = "top"/>
-                    <Card.Body>
-                    <Card.Link href = "#" style = {{'color': 'white'}}>Капитан Марвел</Card.Link>
-                    </Card.Body>
-                </Card>
-                <Card style = {{'color': 'white', 'background': '#141414', 'border': 'solid black', 'width': '12rem'}} className = "mb-3">
-                    <Card.Img src = { movie2 }  variant = "top"/>
-                    <Card.Body>
-                    <Card.Link href = "#" style = {{'color': 'white'}}>Черная вдова</Card.Link>
-                    </Card.Body>
-                </Card>
-                <Card style = {{'color': 'white', 'background': '#141414', 'border': 'solid black', 'width': '12rem'}} className = "mb-3">
-                    <Card.Img src = { movie3 }  variant = "top"/>
-                    <Card.Body>
-                    <Card.Link href = "#" style = {{'color': 'white'}}>Гарри Поттер</Card.Link>
-                    </Card.Body>
-                </Card>
-                <Card style = {{'color': 'white', 'background': '#141414', 'border': 'solid black', 'width': '12rem'}} className = "mb-3">
-                    <Card.Img src = { movie4 }  variant = "top"/>
-                    <Card.Body>
-                    <Card.Link href = "#" style = {{'color': 'white'}}>Сталинград</Card.Link>
-                    </Card.Body>
-                </Card>
-                <Card style = {{'color': 'white', 'background': '#141414', 'border': 'solid black', 'width': '12rem'}} className = "mb-3">
-                    <Card.Img src = { movie5 }  variant = "top"/>
-                    <Card.Body>
-                    <Card.Link href = "#" style = {{'color': 'white'}}>Неудержимые</Card.Link>
-                    </Card.Body>
-                </Card>
-                <Card style = {{'color': 'white', 'background': '#141414', 'border': 'solid black', 'width': '12rem'}} className = "mb-3">
-                    <Card.Img src = { movie6 }  variant = "top"/>
-                    <Card.Body>
-                    <Card.Link href = "#" style = {{'color': 'white'}}>Начало</Card.Link>
-                    </Card.Body>
-                </Card>  
+            {
+                cardInfo.map((card: Icard, index: number) => (<RenderCard card ={ card } key = {index} />))
+            }
+                
         </div>
-    </>    
-)
+    );
+};
+    
 
 export default Recommend
