ace92faff1e9a4133c917b80eef7640f2b68070c
ICX\anastasiya.chultsova <nchulczova@gmail.com>
2021-11-11 18:50:39 +0300

    save

--- src/components/personalAccountPage/PersonalAccount.tsx
+++ src/components/personalAccountPage/PersonalAccount.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react'
-
+import pencil from './icons/pencil.svg'
 import './personalAccount.scss'
 
 
@@ -8,8 +8,24 @@ const PersonalAccount: FC = () => (
     <section className='profile_v'>
         <div className='personal_page' >
             <div className='container'>
-                <h1 style={{paddingTop:"120px", color: "white"}}>{localStorage.getItem('username')}</h1>
-                <h4 style={{color:'white'}}>{localStorage.getItem('email')}</h4> 
+                <div className='profile_menu'>
+                    <div className='profile_menu_info'>
+                        <div> <h1 style={{paddingTop:"120px", color: "white"}}>{localStorage.getItem('username')}</h1> </div>
+                        <div> <h5 style={{color:'gray'}}>{localStorage.getItem('email')}</h5> </div> 
+                    </div>
+                    <div className='profile_menu_item'>
+                    <a href='#'><img src={pencil} className='img_edit'></img>Редактировать</a>
+                    </div>
+                </div>
+            {/* <div>
+                <ul className="profile_menu">
+                    <li>
+                        <a className=''>Смотреть позже</a>
+                    </li>
+
+                </ul>
+            </div>     */}
+                
             </div>    
         </div>
     </section>
